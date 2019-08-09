dacab400ccfe7c9b6263af6cbf20d317c76d79ec
Emil <emilsten@kth.se>
2019-08-09 12:53:47 +0200

    built the first graphql query

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 require('dotenv').config()
 const app = require('kth-node-server')
 const logger = require('./logger')
+
+const expressGraphql = require('express-graphql')
+const { buildSchema } = require('graphql')
+
+// GraphQL schema
+var schema = buildSchema(`
+    type Query {
+        course(id: Int!): Course
+        courses(topic: String): [Course]
+    },
+    type Mutation {
+      createLadokAssignments(id: Int! , sisCourseId: String!): Boolean
+    }
+    type Course {
+        id: Int
+    }
+`)
+
 app.use('/api/lms-sync-courses/', require('./systemroutes'))
+
+app.use('/graphql', expressGraphql({
+  schema: schema,
+  rootValue: {
+    createLadokAssignments: ({ id, sisCourseId }) => {
+      console.log('Helloooo!')
+      return true
+    }
+  },
+  graphiql: true
+}))
+
 app.start({
   port: 3000,
   logger
